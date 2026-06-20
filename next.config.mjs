9720fe822756d03a6749ca532c759f383088c9d2
TonyShotliff <tony.shotliff@gmail.com>
2026-06-20 13:19:19 -0500

    Redirect /garagedoors to homepage, remove unused legacy page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/garagedoors",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
